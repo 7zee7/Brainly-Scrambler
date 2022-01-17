acb7800d50705398fa0f1e0d60ee26cddb1e48a8
7zee7 <97882872+7zee7@users.noreply.github.com>
2022-01-17 10:14:05 +0000

    Update CreateBot.py

--- CreateBot.py
+++ CreateBot.py
@@ -6,7 +6,7 @@ def genRandom(leng):
 
 def macc():
     email = genRandom(28) + '@gmail.com' #bisa di ubah
-    nick = genRandom(6) + 'botbyKIPAS' #bisa di ubah
+    nick = genRandom(6) + '7zee7' #bisa di ubah
     headers = {'Host': 'brainly.co.id','content-length': '346','user-agent': 'Mozilla/5.0 (Linux; Android 10; CPH2239) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.74 Mobile Safari/537.36','content-type': 'application/json','accept': 'application/json','save-data': 'on','sec-ch-ua-platform': 'Android','origin': 'https://brainly.co.id','sec-fetch-site': 'same-origin','sec-fetch-mode': 'cors','sec-fetch-dest': 'empty','referer': 'https://brainly.co.id/signup?entry=1','cookie': 'G_ENABLED_IDPS=google'}
     data = '{"operationName":"registerUser","variables":{"nick":"' + nick + '","country":"ID","entry":"1","email":"' + email + '","password":"yah9898@","dateOfBirth":"2001-01-01","acceptedTermsOfService":true,"accountType":null},"extensions":{"persistedQuery":{"version":1,"sha256Hash":"f737c8e219ac6f050068332b108c70b28a1062722fd6458fe8b49489c9603ca1"}}}'
     response = requests.post('https://brainly.co.id/graphql/id', headers=headers, data=data).text
